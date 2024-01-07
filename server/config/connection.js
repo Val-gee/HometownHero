851cead831701f1acbeca6a16e3248132d06b62b
val.gee <vguevara0806@gmail.com>
2024-01-07 18:33:06 -0500

    added catch err on mongoose connection; created procfile; added process.env.PORT to app.listen in server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dayCampDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
