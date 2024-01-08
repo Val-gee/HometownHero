d27c5b3279b61c79666f8cfedc19e24123c2a582
val.gee <vguevara0806@gmail.com>
2024-01-07 19:41:57 -0500

    removed PORt from app.listen function in server.js

--- server/server.js
+++ server/server.js
@@ -62,7 +62,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
   server.applyMiddleware({ app });
   
   db.once('open', () => {
-    app.listen(PORT || process.env.PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
